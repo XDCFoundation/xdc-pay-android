259589d3d08766f4374798213c8a662893970c02
shaili saini <shaili@parangat.com>
2022-05-13 17:03:48 +0530

    merge

--- app/src/main/java/com/app/xdcpay/Activities/ImportWalletActivity.java
+++ app/src/main/java/com/app/xdcpay/Activities/ImportWalletActivity.java
@@ -146,7 +146,7 @@ public class ImportWalletActivity extends BaseActivity {
                                     accountEntity = new AccountEntity(getResources().getString(R.string.account_1), walletData.getAccountAddress(),
                                             walletData.getPrivateKey(), walletData.getPublickeyKey(), walletData.getSeedPhrase());
                                     new InsertTask(ImportWalletActivity.this, accountEntity).execute();
-                                    SharedPreferenceHelper.setSharedPreferenceString(ImportWalletActivity.this, Constants.ACCOUNT, "0");
+                                    SharedPreferenceHelper.setSharedPreferenceString(ImportWalletActivity.this, Constants.ACCOUNT, "1");
 
 
 
